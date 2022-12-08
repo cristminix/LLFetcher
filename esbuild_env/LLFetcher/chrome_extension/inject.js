ba947230314620c623e3f7520fb57a141c00e545
sutoyocutez <sutoyocutez@gmail.com>
2022-12-08 14:38:53 +0700

    try moving to esbuild environment

---
+++ esbuild_env/LLFetcher/chrome_extension/inject.js
@@ -0,0 +1,2 @@
+"use strict";(()=>{function i(e,c){let r=document.getElementsByTagName(c)[0],t=document.createElement("script");t.setAttribute("type","text/javascript"),t.setAttribute("src",e),r.appendChild(t)}i(chrome.runtime.getURL("create_data_codes.js"),"body");})();
+//# sourceMappingURL=inject.js.map
